24cace01ed3ac3d0883a65a33879088e36bc68b6
mohammad saeid <johndoe@example.com>
2021-05-09 03:39:46 -0400

    initial commit

--- web/models.py
+++ web/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Expecse(models,Model):
-    text = models.Charfield(max_length=255)
-    date = models.DatesTimesFields()
+class Expense(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
     amount = models.BigIntegerField()
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
