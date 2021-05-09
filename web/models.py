bfff93556da685af549b46a4571ab8d55a7a75e8
mohammad saeid <johndoe@example.com>
2021-05-09 02:52:06 -0400

    initial commit

--- web/models.py
+++ web/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Expecse(models,Model):
+    text = models.Charfield(max_length=255)
+    date = models.DatesTimesFields()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User)
